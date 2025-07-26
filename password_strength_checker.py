01bc82c37c5e8088d672139f39559caf55936c63
Ammara Khan <ammarakhan1710@gmail.com>
2025-07-26 13:10:09 +0500

    Add files via upload

---
+++ password_strength_checker.py
@@ -0,0 +1,26 @@
+import re
+
+def check_password_strength(password):
+    score = 0
+    if len(password) >= 8:
+        score += 1
+    if re.search(r"[A-Z]", password):
+        score += 1
+    if re.search(r"[a-z]", password):
+        score += 1
+    if re.search(r"[0-9]", password):
+        score += 1
+    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        score += 1
+
+    if score <= 2:
+        return "Weak"
+    elif score == 3 or score == 4:
+        return "Moderate"
+    else:
+        return "Strong"
+
+if __name__ == "__main__":
+    pwd = input("Enter your password: ")
+    strength = check_password_strength(pwd)
+    print("Your password is:", strength)
